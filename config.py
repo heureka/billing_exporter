9df0e68bb335da45aa00bc4071a0962d02dd373e
Hy3n4 <hy3nk4@gmail.com>
2022-10-18 09:09:31 +0200

    change: switched cortex to mimir

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import time
 from os import getenv
 
-CORTEX = str(getenv("CORTEX"))
+MIMIR = str(getenv("MIMIR"))
+TENANT = str(getenv("TENANT", ""))
 PORT = int(getenv("PORT", 9000))
 REFRESH_FREQUENCY = int(getenv("REFRESH_FREQUENCY", 30))
 LOG_LEVEL = str(getenv("LOG_LEVEL", "warning"))
